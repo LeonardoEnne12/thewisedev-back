dff93f0fab46fb0d9e49408b8c6a3d9748656071
LeonardoEnne12 <enneleonardo@gmail.com>
2022-01-19 17:23:40 -0300

    feat: modules move 19/01 final

--- src/entities/course.ts
+++ src/entities/course.ts
@@ -1,3 +1,4 @@
+import { Lecture } from '.'
 import { Module } from './module'
 import { moveInArray } from './utils'
 
@@ -39,5 +40,12 @@ export class Course {
       return undefined
     }
     return this.modules.indexOf(moduleInCourse) + 1
+  }
+
+  moveLecture (lecture: Lecture, fromModule: Module, toModule: Module, position: number): void {
+    fromModule.remove(lecture)
+    toModule.add(lecture)
+    const currentLectureposition = toModule.position(lecture)
+    if (currentLectureposition !== position) toModule.move(lecture, position)
   }
 }
