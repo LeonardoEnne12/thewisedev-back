7013355f155e4e0358f3c358a0c5d9396a5c4e45
LeonardoEnne12 <enneleonardo@gmail.com>
2022-01-27 14:37:35 -0300

    chore: adding PDFs

--- src/entities/lecture.ts
+++ src/entities/lecture.ts
@@ -1,6 +1,9 @@
+import { Material } from './material'
+
 export class Lecture {
   readonly description: string
   readonly videoUrl: string
+  private readonly materials: Array<Material> = []
 
   constructor (description: string, videoUrl: string) {
     this.description = description
@@ -10,5 +13,18 @@ export class Lecture {
   equals (other: Lecture): boolean {
     return this.description === other.description &&
       this.videoUrl === other.videoUrl
+  }
+
+  add (material: Material): void {
+    this.materials.push(material)
+  }
+
+  remove (material: Material): void {
+    const position = this.materials.indexOf(material)
+    if (position !== -1) this.materials.splice(position, 1)
+  }
+
+  includes (material: Material): boolean {
+    return this.materials.includes(material)
   }
 }
