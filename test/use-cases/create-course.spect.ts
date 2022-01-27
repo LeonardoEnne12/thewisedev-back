a31ee38e452dadd8a9b5d7025fd842c1f2058325
LeonardoEnne12 <enneleonardo@gmail.com>
2022-01-27 15:05:59 -0300

    feat: create-course.spect.ts

---
+++ test/use-cases/create-course.spect.ts
@@ -0,0 +1,7 @@
+/*
+describe('Create course use case', () => {
+    it('should be able to create valid course', () =>{
+        const useCase = new CreateCourse(courseRepository)
+        await useCase.perform(createCourseRequest)
+    })
+}) */
